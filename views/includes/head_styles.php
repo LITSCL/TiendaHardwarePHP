c55c3e08ae760a6b8230c132d171fb055c75542e
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-08 23:44:40 -0300

    Ok

--- views/includes/head_styles.php
+++ views/includes/head_styles.php
@@ -3,4 +3,4 @@
 <link rel="stylesheet" href="<?=BASE_URL?>vendor/jquery-ui-1.13.0/jquery-ui.min.css">
 <link rel="stylesheet" href="<?=BASE_URL?>vendor/jquery-ui-1.13.0/jquery-ui.structure.min.css">
 <link rel="stylesheet" href="<?=BASE_URL?>vendor/jquery-ui-1.13.0/jquery-ui.theme.min.css">
-<link rel="stylesheet" href="<?=BASE_URL?>vendor/fontawesome-free-5.13.0-web/css/all.min.css">
+<link rel="stylesheet" href="<?=BASE_URL?>vendor/fontawesome-free-6.5.0-web/css/all.min.css">
