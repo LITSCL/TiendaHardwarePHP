907d0a9bc34c0768440884023cd97db5dd160745
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 18:27:38 -0300

    Ok

--- SQL-Full/dbtiendahardwarephp.sql
+++ SQL-Full/dbtiendahardwarephp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-04-2023 a las 14:51:48
+-- Tiempo de generación: 10-12-2023 a las 22:18:55
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
