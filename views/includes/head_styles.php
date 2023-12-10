31f7d1ded6e3741cd476575b099e237fe4d6ac2b
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-10 20:25:09 -0300

    Ok

--- views/includes/head_styles.php
+++ views/includes/head_styles.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="<?=BASE_URL?>assets/css/styles.css?<?=TIME_UPDATE_CSS?>"/>
-<link rel="stylesheet" href="<?=BASE_URL?>assets/css/responsive.css?<?=TIME_UPDATE_CSS?>"/>
+<link rel="stylesheet" href="<?=BASE_URL?>assets/css/styles.css?<?=TIME_UPDATE_CSS?>">
+<link rel="stylesheet" href="<?=BASE_URL?>assets/css/responsive.css?<?=TIME_UPDATE_CSS?>">
 <link rel="stylesheet" href="<?=BASE_URL?>vendor/jquery-ui-1.13.0/jquery-ui.min.css">
 <link rel="stylesheet" href="<?=BASE_URL?>vendor/jquery-ui-1.13.0/jquery-ui.structure.min.css">
 <link rel="stylesheet" href="<?=BASE_URL?>vendor/jquery-ui-1.13.0/jquery-ui.theme.min.css">
